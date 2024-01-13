615d38ed809050766efefb768bc88352067581ab
lluuvvii <kurniawanluvi64@gmail.com>
2024-01-13 22:49:06 +0700

    fixing update prompt value issue

--- src/app/update-prompt/page.jsx
+++ src/app/update-prompt/page.jsx
@@ -19,7 +19,7 @@ const EditPrompt = () => {
 
   useEffect(() => {
     const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt${promptId}`)
+      const response = await fetch(`/api/prompt/${promptId}`)
       const data = await response.json()
 
       setPost({

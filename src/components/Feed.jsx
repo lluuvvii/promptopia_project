19122fcda08d35a2269f954a7a4274e2a6497506
lluuvvii <kurniawanluvi64@gmail.com>
2024-04-30 20:51:18 +0700

    fix unsigned in click prompt

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,8 +7,8 @@ export const PromptCardList = ({ data, handleTagClick }) => {
 
   return (
     <div className='mt-16 prompt_layout'>
-      {data.map(post => (
-        <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+      {data.map((post, i) => (
+        <PromptCard key={i} post={post} handleTagClick={handleTagClick} />
       ))}
     </div>
   )
